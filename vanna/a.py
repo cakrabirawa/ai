bb98d322ce70f340632480735cd63b0ca5418ebf
Edwar Rinaldo <labirin69@gmail.com>
2024-11-17 13:06:29 +0700

    Added vanna ai

--- vanna/a.py
+++ vanna/a.py
@@ -14,17 +14,57 @@ vn.connect_to_mysql(host='localhost', dbname='openposcinunukdb2', user='root', p
 df_information_schema = vn.run_sql("SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'openposcinunukdb2'")
 
 plan = vn.get_training_plan_generic(df_information_schema)
-plan
+vn.train(plan=plan)
 
 vn.train(ddl="""
-    CREATE TABLE IF NOT EXISTS tx_open_pos_d (
-        id INT PRIMARY KEY,
-        name VARCHAR(100),
-        age INT
-    )
+    CREATE TABLE `tx_open_pos_d` (
+  `sIdTXOpenPOS_fk` varchar(13) NOT NULL,
+  `nIdTXType_fk` int(11) NOT NULL,
+  `nIdStore_fk` int(11) NOT NULL,
+  `sIdProduk_fk` varchar(20) NOT NULL,
+  `nQtyJual` int(11) DEFAULT NULL,
+  `nHargaJual` double DEFAULT NULL,
+  `nDisc` float DEFAULT NULL,
+  `nDiscAmount` double DEFAULT NULL,
+  `nSubTotal` double DEFAULT NULL,
+  `nGrossAmount` double DEFAULT NULL,
+  `nIdSuplier_fk` int(11) NOT NULL,
+  `nRecvQtyDoc` int(11) DEFAULT NULL,
+  `nRecvQtyFisik` int(11) DEFAULT NULL,
+  `nRecvQtySelisih` int(11) DEFAULT NULL,
+  `nRecvQtyMod` int(11) DEFAULT NULL,
+  `nSeqNo` int(11) DEFAULT NULL,
+  `sRecvDocNoReff` varchar(20) DEFAULT NULL,
+  `dRecvDocNoReff` date DEFAULT NULL,
+  `nQtySewa` int(11) DEFAULT NULL COMMENT 'Sewa',
+  `nQtySewaMod` int(11) DEFAULT NULL COMMENT 'Sewa',
+  `nQtyKembali` int(11) DEFAULT NULL COMMENT 'Kembali',
+  `nHargaPokokSewa` double DEFAULT NULL COMMENT 'Sewa',
+  `nHargaSewa` double DEFAULT NULL COMMENT 'Sewa',
+  `nHargaJaminanSewa` double DEFAULT NULL COMMENT 'Sewa',
+  `nPersenSewa` float DEFAULT NULL COMMENT 'Sewa',
+  `nLamaSewa` int(11) DEFAULT NULL COMMENT 'Sewa',
+  `nSubTotalSewa` double DEFAULT NULL COMMENT 'Sewa',
+  `nLamaTelat` int(11) DEFAULT NULL COMMENT 'Kembali',
+  `nNilaiDenda` double DEFAULT NULL COMMENT 'Kembali',
+  `nPersenDenda` float DEFAULT NULL COMMENT 'Kembali',
+  `sIdTXOpenPOSReffSewa_fk` varchar(13) DEFAULT NULL COMMENT 'Kembali',
+  `nFisikKembali` int(11) DEFAULT NULL COMMENT 'Kembali',
+  `sCreateBy` varchar(50) DEFAULT NULL,
+  `dCreateOn` datetime DEFAULT NULL,
+  `sLastEditBy` varchar(50) DEFAULT NULL,
+  `dLastEditOn` datetime DEFAULT NULL,
+  `sDeleteBy` varchar(50) DEFAULT NULL,
+  `dDeleteOn` datetime DEFAULT NULL,
+  `sStatusDelete` char(1) DEFAULT NULL,
+  `sUUID` varchar(36) NOT NULL,
+  `nIdDepartemenProduk_fk` int(11) NOT NULL,
+  `sNamaDepartemenProduk` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`sIdTXOpenPOS_fk`,`nIdTXType_fk`,`nIdStore_fk`,`sIdProduk_fk`,`nIdSuplier_fk`,`sUUID`,`nIdDepartemenProduk_fk`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 """)
 
-#vn.train(documentation="Our business defines OTIF score as the percentage of orders that are delivered on time and in full")
+vn.train(documentation="Our Store Sales Data")
 
 vn.train(sql="SELECT * FROM tx_open_pos_d")
 
