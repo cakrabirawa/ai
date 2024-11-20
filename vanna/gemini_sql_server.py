1368caddf583ac621ed7b4263c5e2568e6c3af2b
Edwar Rinaldo <labirin69@gmail.com>
2024-11-20 16:11:49 +0700

    Modified some code

--- vanna/gemini_sql_server.py
+++ vanna/gemini_sql_server.py
@@ -54,45 +54,10 @@ def format_results(results):
 
 # Sample prompt for the Generative AI model
 prompt = """
-    You are an expert in converting English or Indonesia questions to MS SQL query!
-    The SQL database has the name test01 and has the following columns - dataAreaId,
-      TransactionNumber,
-      OperatingUnitNumber,
-      Terminal,
-      RreceiptId,
-      DiscountAmount,
-      NetPrice,
-      TotalManualDiscountPercentage,
-      CustomerAccount,
-      TransactionOrderType,
-      CostAmount,
-      AmountPostedToAccount,
-      TransactionDate,
-      PaymentAmount,
-      TransactionStatus,
-      NumberOfPaymentLines,
-      Warehouse,
-      CustomerName,
-      NetAmount,
-      Staff,
-      CustomerDiscountAmount,
-      SalesInvoiceAmount,
-      TotalDiscountAmount,
-      DiscountAmountWithoutTax,
-      CreatedOnPosTerminal,
-      SalesOrderAmount,
-      TotalManualDiscountAmount,
-      TransactionType,
-      TransactionTime,
-      SalesPaymentDifference 
-       \n\nFor example, \nExample 1 - How many entries of records are present?, 
-    the SQL command will be something like this SELECT COUNT(*) FROM test01;
-    \nExample 2 - Tell me all the terminal?, 
-    the SQL command will be something like this SELECT * FROM test01 where Terminal= '10130-71'; 
-    also the sql code should not have ``` in beginning or end and sql word in output
+    terjemahkan pertanyaan ke dalam bahasa indonesia, lakukan konversi pertanyaan ke dalam struktur sql, output juga menyertakan lama pencarian, saya memiliki tabel test01, test02, tempe01 dan 365_loging
     """
 # Natural language query to be converted to SQL as input to Gemini
-user_query = "Show top 10 sales"
+user_query = "tampilkan 5 penjualan terakhir lengkap dengan id produk dan nama produk"
 
 # Generate SQL query from the natural language query using the Generative AI model
 response = model.generate_content([prompt[0], user_query])
@@ -100,20 +65,20 @@ response = model.generate_content([prompt[0], user_query])
 print (response.text)
 
 # Execute the generated SQL query
-results = execute_query(response.text.replace("```", "").replace("sql", ""))
+#results = execute_query(response.text.replace("```", "").replace("sql", ""))
 
-print(results)
+#print(results)
 #unformatted results from the database
 
 # Format and print the query results
-formatted_results = format_results(results)
+#formatted_results = format_results(results)
 
-print("Query Results:")
-for row in formatted_results:
-    print("------------------------------------")
-    for key, value in row.items():
-        print(f"{key}: {value}")
-    print("------------------------------------")
+#print("Query Results:")
+#for row in formatted_results:
+#    print("------------------------------------")
+#    for key, value in row.items():
+#        print(f"{key}: {value}")
+#    print("------------------------------------")
 
 # Close the database connection
 conn.close()
